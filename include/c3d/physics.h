66fd16f7f875e84cf307246a57a5048f59faba65
Thompson Lee <tom.mai78101@gmail.com>
2016-09-06 23:44:03 -0400

    Separated the AABBs to its own file.
    Added non-self-documenting comments that defines each function.

--- include/c3d/physics.h
+++ include/c3d/physics.h
@@ -8,58 +8,66 @@ typedef struct C3D_AABB {
 	C3D_FVec max;
 } C3D_AABB;
 
-static inline bool AABB_ContainsAABB(const C3D_AABB* myself, const C3D_AABB* other) {
-	return myself->min.x <= other->min.x &&
-	       myself->min.y <= other->min.y &&
-		   myself->min.z <= other->min.z &&
-		   myself->max.x >= other->max.x &&
-		   myself->max.y >= other->max.y &&
-		   myself->max.z >= other->max.z;
-}
+typedef struct C3D_HalfSpace {
+	C3D_FVec normal;
+	float distance;
+} C3D_HalfSpace;
 
-static inline bool AABB_ContainsFVec3(const C3D_AABB* myself, const C3D_FVec* point) {
-	return myself->min.x <= point->x &&
-	       myself->min.y <= point->y &&
-		   myself->min.z <= point->z &&
-		   myself->max.x >= point->x &&
-		   myself->max.y >= point->y &&
-		   myself->max.z >= point->z;
-}
+typedef struct C3D_RaycastData {
+	C3D_FVec rayOrigin;
+	C3D_FVec direction;
+	float deltaTime;
+	float timeOfImpact;
+	C3D_FVec normal;
+} C3D_RaycastData;
 
-static inline float AABB_GetSurfaceArea(const C3D_AABB* myself) {
-	float x = myself->max.x - myself->min.x;
-	float y = myself->max.y - myself->min.y;
-	float z = myself->max.z - myself->min.z;
-	return (float) (2.0f) * (x * x + x * z + y * z);
-}
+/***********************************************
+ * AABB Box Collision Helper Functions 
+ ***********************************************/
 
-static inline void AABB_Combine(C3D_AABB* out, const C3D_AABB* a, const C3D_AABB* b){
-	out->min = FVec3_New((a->min.x < b->min.x ? a->min.x : b->min.x), (a->min.y < b->min.y ? a->min.y : b->min.y), (a->min.z < b->min.z ? a->min.z : b->min.z));
-	out->max = FVec3_New((a->max.x > b->max.x ? a->max.x : b->max.x), (a->max.y > b->max.y ? a->max.y : b->max.y), (a->max.z > b->max.z ? a->max.z : b->max.z));
-}
+/**
+ * @brief Checks if inner AABB box is within the outer AABB box.
+ * @param[in]   outer   Outer AABB box.
+ * @param[in]   inner    Inner AABB box to compare with.
+ * @return True if outer AABB box contains an inner AABB box. False, if otherwise. 
+ */
+bool AABB_ContainsAABB(const C3D_AABB* outer, const C3D_AABB* inner);
 
-static inline bool AABB_CollidesAABB(const C3D_AABB* a, const C3D_AABB* b){
-	if (a->max.x < b->min.x || a->min.x > b->max.x)
-		return false;
-	if (a->max.y < b->min.y || a->min.y > b->max.y)
-		return false;
-	if (a->max.z < b->min.z || a->min.z > b->max.z)
-		return false;
-	return true;
-}
+/**
+ * @brief Checks if C3D_FVec point is within the AABB box.
+ * @param[in]   outer   Outer AABB box.
+ * @param[in]   inner   C3D_FVec vector position.
+ * @return True if vector position is within the AABB box. False, if otherwise.
+ */
+bool AABB_ContainsFVec3(const C3D_AABB* outer, const C3D_FVec* inner);
 
-// http://box2d.org/2014/02/computing-a-basis/
-static inline void ComputeBasis(const C3D_FVec* a, C3D_FVec* b, C3D_FVec* c)
-{
-	// Suppose vector a has all equal components and is a unit vector: a = (s, s, s)
-	// Then 3*s*s = 1, s = sqrt(1/3) = 0.57735027. This means that at least one component of a
-	// unit vector must be greater or equal to 0.57735027. Can use SIMD select operation.
+/**
+ * @brief Obtain the surface area of the AABB box specified.
+ * @param[in]  myself   The AABB box for finding the surface area.
+ * @return The surface area.
+ */
+float AABB_GetSurfaceArea(const C3D_AABB* myself);
 
-	if (fabsf(a->x) >= 0.57735027f)
-		*b = FVec3_New(a->y, -a->x, 0.0f);
-	else
-		*b = FVec3_New(0.0f, a->z, -a->y);
+/**
+ * @brief Unionize two AABB boxes to create 1 bigger AABB box that contains the two AABB boxes.
+ * @param[out]   out   The resulting larger AABB box.
+ * @param[in]    a     The first AABB box.
+ * @param[in]    b     The second AABB box.
+ */
+void AABB_Combine(C3D_AABB* out, const C3D_AABB* a, const C3D_AABB* b);
 
-	*b = FVec3_Normalize(*b);
-	*c = FVec3_Cross(*a, *b);
-}
+/**
+ * @brief Checks if the two AABB boxes intersects each other.
+ * @param[in]    a     The first AABB box.
+ * @param[in]    b     The second AABB box.
+ * @return True if both boxes intersect each other. False, if otherwise.
+ */
+bool AABB_CollidesAABB(const C3D_AABB* a, const C3D_AABB* b);
+
+/**
+ * @brief See: http://box2d.org/2014/02/computing-a-basis/
+ * @param[in]   a   A unit vector.
+ * @param[out]  b   A unit vector perpendicular to unit vector, "a".
+ * @param[out]  c   A unit vector perpendicular to unit vectors, "a" and "b".
+ */ 
+void ComputeBasis(const C3D_FVec* a, C3D_FVec* b, C3D_FVec* c);
---
+++ source/physics/aabb.c
@@ -0,0 +1,57 @@
+#include <c3d/physics.h>
+
+bool AABB_ContainsAABB(const C3D_AABB* outer, const C3D_AABB* inner) {
+	return outer->min.x <= inner->min.x &&
+	       outer->min.y <= inner->min.y &&
+		   outer->min.z <= inner->min.z &&
+		   outer->max.x >= inner->max.x &&
+		   outer->max.y >= inner->max.y &&
+		   outer->max.z >= inner->max.z;
+}
+
+bool AABB_ContainsFVec3(const C3D_AABB* outer, const C3D_FVec* inner) {
+	return outer->min.x <= inner->x &&
+	       outer->min.y <= inner->y &&
+		   outer->min.z <= inner->z &&
+		   outer->max.x >= inner->x &&
+		   outer->max.y >= inner->y &&
+		   outer->max.z >= inner->z;
+}
+
+float AABB_GetSurfaceArea(const C3D_AABB* myself) {
+	float x = myself->max.x - myself->min.x;
+	float y = myself->max.y - myself->min.y;
+	float z = myself->max.z - myself->min.z;
+	return (float) (2.0f) * (x * x + x * z + y * z);
+}
+
+void AABB_Combine(C3D_AABB* out, const C3D_AABB* a, const C3D_AABB* b){
+	out->min = FVec3_New((a->min.x < b->min.x ? a->min.x : b->min.x), (a->min.y < b->min.y ? a->min.y : b->min.y), (a->min.z < b->min.z ? a->min.z : b->min.z));
+	out->max = FVec3_New((a->max.x > b->max.x ? a->max.x : b->max.x), (a->max.y > b->max.y ? a->max.y : b->max.y), (a->max.z > b->max.z ? a->max.z : b->max.z));
+}
+
+bool AABB_CollidesAABB(const C3D_AABB* a, const C3D_AABB* b){
+	if (a->max.x < b->min.x || a->min.x > b->max.x)
+		return false;
+	if (a->max.y < b->min.y || a->min.y > b->max.y)
+		return false;
+	if (a->max.z < b->min.z || a->min.z > b->max.z)
+		return false;
+	return true;
+}
+
+// http://box2d.org/2014/02/computing-a-basis/
+void ComputeBasis(const C3D_FVec* a, C3D_FVec* b, C3D_FVec* c)
+{
+	// Suppose vector a has all equal components and is a unit vector: a = (s, s, s)
+	// Then 3*s*s = 1, s = sqrt(1/3) = 0.57735027. This means that at least one component of a
+	// unit vector must be greater or equal to 0.57735027. Can use SIMD select operation.
+
+	if (fabsf(a->x) >= 0.57735027f)
+		*b = FVec3_New(a->y, -a->x, 0.0f);
+	else
+		*b = FVec3_New(0.0f, a->z, -a->y);
+
+	*b = FVec3_Normalize(*b);
+	*c = FVec3_Cross(*a, *b);
+}
