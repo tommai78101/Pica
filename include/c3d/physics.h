03fe95d14ad2c4e8f2ee1f0cbd0bec6443e34342
Thompson Lee <tom.mai78101@gmail.com>
2016-09-13 17:14:35 -0400

    Separating Box functions. Working on broadphases.

--- include/c3d/physics.h
+++ include/c3d/physics.h
@@ -481,51 +481,7 @@ static inline void Box_SetSensorFlag(C3D_Box* box, bool flag)
  * TODO: Check to see what this function really does.
  * @note Randy Gaul: The entire function performs box to ray and finds the hit point. Using the transpose lets one solve ray to AABB and still get the correct results. Ray to AABB is easier than ray to OBB.
  */
-bool Box_Raycast(C3D_Box* box, const C3D_Transform* transform, C3D_RaycastData* raycastData)
-{
-	C3D_Transform worldTransform;
-	Transform_Multiply(&worldTransform, transform, &box->localTransform);
-	C3D_FVec direction;
-	Transform_MultiplyTransposeFVec(&direction, &worldTransform.rotation, &raycastData->direction);
-	C3D_FVec position;
-	Transform_MultiplyTransformFVec(&position, &worldTransform, &raycastData->rayOrigin);
-	float minimumTime = 0.0f;
-	float maximumTime = raycastData->endPointTime;
-	float time0Value;  //Point at t = 0 (unit)
-	float time1Value;  //Point at t = 1 (unit)
-	C3D_FVec normal0;
-	for (int i = 3; i > 0; i--)
-	{
-		//C3D_FVec is structured as WZYX, so the index goes from 3 -> 2 -> 1 -> break.
-		if (fabsf(direction.c[i]) < FLT_EPSILON)
-		{
-			if (position.c[i] < -box->extent.c[i] || position.c[i] > box->extent.c[i])
-				return false;
-		}
-		else 
-		{
-			float inverseDirection = 1.0f / direction.c[i];
-			float sign = (direction.c[i] >= 0.0f ? 1.0f : -1.0f);
-			float extentValue = box->extent.c[i] * sign;
-			C3D_FVec normal = {};
-			normal.c[i] = -sign;
-			
-			time0Value = -(extentValue + position.c[i]) * inverseDirection;
-			time1Value = (extentValue - position.c[i]) * inverseDirection;
-			if (time0Value > minimumTime)
-			{
-				normal0 = normal;
-				minimumTime = time0Value;
-			}
-			maximumTime = (maximumTime < time1Value ? maximumTime : time1Value);
-			if (minimumTime > maximumTime)
-				return false;
-		}
-	}
-	raycastData->normal = Mtx_MultiplyFVec3(&worldTransform.rotation, normal0);
-	raycastData->timeOfImpact = minimumTime;
-	return true;
-}
+bool Box_Raycast(C3D_Box* box, const C3D_Transform* transform, C3D_RaycastData* raycastData);
 
 /**
  * @brief Using the given C3D_Box object data, create a C3D_AABB object that encapsulate the C3D_Box data.
@@ -533,77 +489,81 @@ bool Box_Raycast(C3D_Box* box, const C3D_Transform* transform, C3D_RaycastData* 
  * @param[in]      box            The C3D_Box object to derive C3D_AABB object from.
  * @param[in]      transform      The C3D_Transform object's transform properties to convert from local transform space to world transform space.
  */
-void Box_ComputeAABB(C3D_AABB* aabb, C3D_Box* box, const C3D_Transform* transform)
-{
-	C3D_Transform worldTransform;
-	Transform_Multiply(&worldTransform, transform, &box->localTransform);
-	C3D_FVec vectors[8] = 
-	{
-		FVec3_New(-box->extent.x, -box->extent.y, -box->extent.z),
-		FVec3_New(-box->extent.x, -box->extent.y,  box->extent.z),
-		FVec3_New(-box->extent.x,  box->extent.y, -box->extent.z),
-		FVec3_New(-box->extent.x,  box->extent.y,  box->extent.z),
-		FVec3_New( box->extent.x, -box->extent.y, -box->extent.z),
-		FVec3_New( box->extent.x, -box->extent.y,  box->extent.z),
-		FVec3_New( box->extent.x,  box->extent.y, -box->extent.z),
-		FVec3_New( box->extent.x,  box->extent.y,  box->extent.z)
-	};
-	for (int i = 0; i < 8; i++)
-		Transform_MultiplyTransformFVec((vectors + i), &worldTransform, (vectors + i));
-	C3D_FVec minimum = FVec3_New(FLT_MAX, FLT_MAX, FLT_MAX);
-	C3D_FVec maximum = FVec3_New(-FLT_MAX, -FLT_MAX, -FLT_MAX);
-	for (int i = 0; i < 8; i++)
-	{
-		minimum = FVec3_Min(minimum, vectors[i]);
-		maximum = FVec3_Max(maximum, vectors[i]);
-	}
-	aabb->min = minimum;
-	aabb->max = maximum;
-}
+void Box_ComputeAABB(C3D_AABB* aabb, C3D_Box* box, const C3D_Transform* transform);
 
 /**
  * @brief Using the given C3D_Box data, compute and obtain the mass.
  * @param[out]   out     The resulting C3D_MassData object.
  * @param[in]    box     The C3D_Box data to compute.
  */
-void Box_ComputeMass(C3D_MassData* out, C3D_Box* box)
+void Box_ComputeMass(C3D_MassData* out, C3D_Box* box);
+
+/**************************************************
+ * Contact Manager Functions
+ **************************************************/
+
+typedef struct C3D_ContactManager 
 {
-	float squaredExtentX = 4.0f * box->extent.x * box->extent.x;
-	float squaredExtentY = 4.0f * box->extent.y * box->extent.y;
-	float squaredExtentZ = 4.0f * box->extent.z * box->extent.z;
-	float mass = 8.0f * box->extent.x * box->extent.y * box->extent.z * box->density;
-	float newX = (1.0f / 12.0f) * mass * (squaredExtentY + squaredExtentZ);
-	float newY = (1.0f / 12.0f) * mass * (squaredExtentX + squaredExtentZ);
-	float newZ = (1.0f / 12.0f) * mass * (squaredExtentX + squaredExtentY);
 	
-	C3D_Mtx inertiaMatrix;
-	Mtx_Diagonal(&inertiaMatrix, newX, newY, newZ, 1.0f);
+} C3D_ContactManager;
+
+/**************************************************
+ * Broadphase
+ **************************************************/
+
+typedef struct C3D_ContactPair 
+{
+	unsigned int A;
+	unsigned int B;
+} C3D_ContactPair;
+
+typedef struct C3D_BroadPhase 
+{
+	C3D_ContactManager* contactManager;
+	C3D_ContactPair* pairBuffer;
+	unsigned int pairCount;
+	unsigned int pairCapacity;
+	unsigned int* moveBuffer;
+	unsigned int moveCount;
+	unsigned int moveCapacity;
+} C3D_BroadPhase;
+
+void Broadphase_Init(C3D_BroadPhase* out, C3D_ContactManager* contactManager)
+{
 	
-	C3D_Mtx transposedRotationMatrix;
-	Mtx_Copy(&transposedRotationMatrix, &box->localTransform.rotation);
-	Mtx_Transpose(&transposedRotationMatrix);
-	
-	C3D_Mtx temp;
-	Mtx_Multiply(&temp, &inertiaMatrix, &transposedRotationMatrix);
-	Mtx_Multiply(&inertiaMatrix, &box->localTransform.rotation, &temp);
-	
-	C3D_Mtx identity;
-	Mtx_Identity(&identity);
-	
-	float dotPosition = FVec3_Dot(box->localTransform.position, box->localTransform.position);
-	Mtx_Scale(&identity, dotPosition, dotPosition, dotPosition);
-	
-	C3D_Mtx outerProduct;
-	Mtx_OuterProduct(&outerProduct, &box->localTransform.position, &box->localTransform.position);
-	
-	Mtx_Subtract(&temp, &identity, &outerProduct);
-	Mtx_Scale(&temp, mass, mass, mass);
-	Mtx_Add(&inertiaMatrix, &inertiaMatrix, &temp);
-	
-	out->center = box->localTransform.position;
-	Mtx_Copy(&out->inertia, &inertiaMatrix);
-	out->mass = mass;
 }
+
+/**************************************************
+ * Dynamic AABB Tree Functions
+ **************************************************/
+
+static const int TREENODE_NULL = -1;
+
+typedef struct C3D_DynamicAABBTreeNode 
+{
+	C3D_AABB aabb;
+	union 
+	{
+		unsigned int parent;
+		unsigned int next;
+	};
+	struct 
+	{
+		unsigned int left;
+		unsigned int right;
+	};
+	void* userData;
+	unsigned int height;
+} C3D_DynamicAABBTreeNode;
+
+typedef struct C3D_DynamicAABBTree 
+{
+	unsigned int root;
+	C3D_DynamicAABBTreeNode* nodes;
+	unsigned int count;
+	unsigned int capacity;
+	unsigned int freeList;
+} C3D_DynamicAABBTree;
 
 /**************************************************
  * Physics Body Functions.
@@ -748,17 +708,9 @@ typedef struct C3D_ContactConstraint
 	unsigned int flags;
 } C3D_ContactConstraint;
 
-
 /**************************************************
  * Scene Functions
  **************************************************/
-
-typedef struct C3D_ContactManager 
-{
-	
-} C3D_ContactManager;
-
-
 
 typedef struct C3D_Scene 
 {
