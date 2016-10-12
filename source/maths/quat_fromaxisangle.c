3922db0f8d12945a5ccca4b7cab43bd73b14b7ab
Thompson Lee <tom.mai78101@gmail.com>
2016-10-12 00:50:04 -0400

    Testing for creating a submodule.

--- source/maths/quat_fromaxisangle.c
+++
@@ -1,9 +0,0 @@
-#include <c3d/maths.h>
-
-C3D_FQuat Quat_FromAxisAngle(C3D_FVec axis, float angle)
-{
-	float halfAngle = angle / 2.0f;
-	float scale = sinf(halfAngle);
-	axis = Quat_Normalize(axis);
-	return Quat_New(axis.x * scale, axis.y * scale, axis.z * scale, cosf(halfAngle));
-}
